9f8fbe132f248d938306b21db4de8dbbdd7357a0
Sanso95 <marco.sansoni.1995@gmail.com>
2020-03-03 21:53:02 +0100

    re build

---
+++ demo/precache-manifest.f6c62b89c47fece8f82845a8f5861916.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42c5fbf4c5598f266d05ae9a81c9e31",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.1080384b75005fb86fac.bundle.js"
+  },
+  {
+    "url": "runtime~main.1080384b75005fb86fac.bundle.js"
+  },
+  {
+    "revision": "3d1e36d0a3c3f054561d",
+    "url": "static/css/vendors~main.18fb2ca0.chunk.css"
+  },
+  {
+    "url": "vendors~main.1080384b75005fb86fac.bundle.js"
+  },
+  {
+    "url": "vendors~main.1080384b75005fb86fac.bundle.js.LICENSE.txt"
+  }
+]);
